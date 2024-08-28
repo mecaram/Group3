f23255e442ae02e1e345f2d6fdfbd56d3b5b6b85
Thiago Lozano <thiagolozano47@gmail.com>
2024-08-27 21:26:21 -0300

    Puse botones

--- GestorMenu.Designer.cs
+++ GestorMenu.Designer.cs
@@ -31,13 +31,23 @@
             btnClientes = new Button();
             btnProveedores = new Button();
             btnProductos = new Button();
+            btnCiudades = new Button();
+            btnDetallesDeVentas = new Button();
+            btnMediosDePagos = new Button();
+            btnMarcas = new Button();
+            btnCompras = new Button();
+            btnRubros = new Button();
+            btnVentas = new Button();
+            btnCierres = new Button();
+            btnAdministradores = new Button();
             SuspendLayout();
             // 
             // btnClientes
             // 
-            btnClientes.Location = new Point(39, 33);
+            btnClientes.Location = new Point(45, 44);
+            btnClientes.Margin = new Padding(3, 4, 3, 4);
             btnClientes.Name = "btnClientes";
-            btnClientes.Size = new Size(90, 44);
+            btnClientes.Size = new Size(103, 59);
             btnClientes.TabIndex = 0;
             btnClientes.Text = "Clientes";
             btnClientes.UseVisualStyleBackColor = true;
@@ -45,9 +55,10 @@
             // 
             // btnProveedores
             // 
-            btnProveedores.Location = new Point(153, 33);
+            btnProveedores.Location = new Point(296, 44);
+            btnProveedores.Margin = new Padding(3, 4, 3, 4);
             btnProveedores.Name = "btnProveedores";
-            btnProveedores.Size = new Size(90, 44);
+            btnProveedores.Size = new Size(103, 59);
             btnProveedores.TabIndex = 1;
             btnProveedores.Text = "Proveedores";
             btnProveedores.UseVisualStyleBackColor = true;
@@ -55,24 +66,126 @@
             // 
             // btnProductos
             // 
-            btnProductos.Location = new Point(263, 33);
+            btnProductos.Location = new Point(510, 44);
+            btnProductos.Margin = new Padding(3, 4, 3, 4);
             btnProductos.Name = "btnProductos";
-            btnProductos.Size = new Size(90, 44);
+            btnProductos.Size = new Size(103, 59);
             btnProductos.TabIndex = 2;
             btnProductos.Text = "Productos";
             btnProductos.UseVisualStyleBackColor = true;
             btnProductos.Click += btnProductos_Click;
             // 
+            // btnCiudades
+            // 
+            btnCiudades.Location = new Point(719, 44);
+            btnCiudades.Name = "btnCiudades";
+            btnCiudades.Size = new Size(94, 59);
+            btnCiudades.TabIndex = 3;
+            btnCiudades.Text = "Ciudades";
+            btnCiudades.UseVisualStyleBackColor = true;
+            btnCiudades.Click += btnCiudades_Click;
+            // 
+            // btnDetallesDeVentas
+            // 
+            btnDetallesDeVentas.Location = new Point(46, 398);
+            btnDetallesDeVentas.Name = "btnDetallesDeVentas";
+            btnDetallesDeVentas.Size = new Size(169, 59);
+            btnDetallesDeVentas.TabIndex = 4;
+            btnDetallesDeVentas.Text = "Detalles de Ventas";
+            btnDetallesDeVentas.UseVisualStyleBackColor = true;
+            btnDetallesDeVentas.Click += btnDetallesDeVentas_Click;
+            // 
+            // btnMediosDePagos
+            // 
+            btnMediosDePagos.Location = new Point(526, 398);
+            btnMediosDePagos.Name = "btnMediosDePagos";
+            btnMediosDePagos.Size = new Size(182, 59);
+            btnMediosDePagos.TabIndex = 5;
+            btnMediosDePagos.Text = "Medios de Pagos";
+            btnMediosDePagos.UseVisualStyleBackColor = true;
+            btnMediosDePagos.Click += btnMediosDePagos_Click;
+            // 
+            // btnMarcas
+            // 
+            btnMarcas.Location = new Point(510, 149);
+            btnMarcas.Name = "btnMarcas";
+            btnMarcas.Size = new Size(103, 59);
+            btnMarcas.TabIndex = 6;
+            btnMarcas.Text = "Marcas";
+            btnMarcas.UseVisualStyleBackColor = true;
+            btnMarcas.Click += btnMarcas_Click;
+            // 
+            // btnCompras
+            // 
+            btnCompras.Location = new Point(46, 149);
+            btnCompras.Name = "btnCompras";
+            btnCompras.Size = new Size(102, 59);
+            btnCompras.TabIndex = 7;
+            btnCompras.Text = "Compras";
+            btnCompras.UseVisualStyleBackColor = true;
+            btnCompras.Click += btnCompras_Click;
+            // 
+            // btnRubros
+            // 
+            btnRubros.Location = new Point(295, 276);
+            btnRubros.Name = "btnRubros";
+            btnRubros.Size = new Size(103, 59);
+            btnRubros.TabIndex = 8;
+            btnRubros.Text = "Rubros";
+            btnRubros.UseVisualStyleBackColor = true;
+            btnRubros.Click += btnRubros_Click;
+            // 
+            // btnVentas
+            // 
+            btnVentas.Location = new Point(46, 276);
+            btnVentas.Name = "btnVentas";
+            btnVentas.Size = new Size(103, 59);
+            btnVentas.TabIndex = 9;
+            btnVentas.Text = "Ventas";
+            btnVentas.UseVisualStyleBackColor = true;
+            btnVentas.Click += btnVentas_Click;
+            // 
+            // btnCierres
+            // 
+            btnCierres.Location = new Point(296, 149);
+            btnCierres.Name = "btnCierres";
+            btnCierres.Size = new Size(102, 59);
+            btnCierres.TabIndex = 10;
+            btnCierres.Text = "Cierres";
+            btnCierres.UseVisualStyleBackColor = true;
+            btnCierres.Click += btnCierres_Click;
+            // 
+            // btnAdministradores
+            // 
+            btnAdministradores.Location = new Point(295, 398);
+            btnAdministradores.Name = "btnAdministradores";
+            btnAdministradores.Size = new Size(162, 59);
+            btnAdministradores.TabIndex = 11;
+            btnAdministradores.Text = "Administradores";
+            btnAdministradores.UseVisualStyleBackColor = true;
+            btnAdministradores.Click += btnAdministradores_Click;
+            // 
             // GestorMenu
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(btnAdministradores);
+            Controls.Add(btnCierres);
+            Controls.Add(btnVentas);
+            Controls.Add(btnRubros);
+            Controls.Add(btnCompras);
+            Controls.Add(btnMarcas);
+            Controls.Add(btnMediosDePagos);
+            Controls.Add(btnDetallesDeVentas);
+            Controls.Add(btnCiudades);
             Controls.Add(btnProductos);
             Controls.Add(btnProveedores);
             Controls.Add(btnClientes);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "GestorMenu";
             Text = "Menú Principal - Gestion";
+            Load += GestorMenu_Load;
             ResumeLayout(false);
         }
 
@@ -81,5 +194,14 @@
         private Button btnClientes;
         private Button btnProveedores;
         private Button btnProductos;
+        private Button btnCiudades;
+        private Button btnDetallesDeVentas;
+        private Button btnMediosDePagos;
+        private Button btnMarcas;
+        private Button btnCompras;
+        private Button btnRubros;
+        private Button btnVentas;
+        private Button btnCierres;
+        private Button btnAdministradores;
     }
 }
--- GestorMenu.cs
+++ GestorMenu.cs
@@ -24,5 +24,64 @@ namespace Gestion
             Productos productos = new Productos();
             productos.ShowDialog();
         }
+
+        private void GestorMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMarcas_Click(object sender, EventArgs e)
+        {
+            Marcas marcas = new Marcas();
+            marcas.ShowDialog();
+        }
+
+        private void btnRubros_Click(object sender, EventArgs e)
+        {
+            Rubros rubros = new Rubros();
+            rubros.ShowDialog();
+        }
+
+        private void btnCiudades_Click(object sender, EventArgs e)
+        {
+            Ciudades ciudades = new Ciudades();
+            ciudades.ShowDialog();
+        }
+
+        private void btnMediosDePagos_Click(object sender, EventArgs e)
+        {
+            MediosDePagos mediosDePagos = new MediosDePagos();
+            mediosDePagos.ShowDialog();
+        }
+
+        private void btnCompras_Click(object sender, EventArgs e)
+        {
+            Compras compras = new Compras();
+            compras.ShowDialog();
+        }
+
+        private void btnCierres_Click(object sender, EventArgs e)
+        {
+            Cierres cierres = new Cierres();
+            cierres.ShowDialog();
+        }
+
+        private void btnVentas_Click(object sender, EventArgs e)
+        {
+            Ventas ventas = new Ventas();
+            ventas.ShowDialog();
+        }
+
+        private void btnDetallesDeVentas_Click(object sender, EventArgs e)
+        {
+            DetallesDeVentas detallesDeVentas = new DetallesDeVentas();
+            detallesDeVentas.ShowDialog();
+        }
+
+        private void btnAdministradores_Click(object sender, EventArgs e)
+        {
+            Administradores administradores = new Administradores();    
+                administradores.ShowDialog();   
+        }
     }
 }
