0479afc244a8e63e4234f3fdbb6a85d69f72c539
roddy jara <roddyasd@gmail.com>
2024-08-27 21:48:18 -0300

    Revert "Actualizacion Formulario de Productos"
    This reverts commit 9bb2fb440d3a8f0b9fbe4209de82bc86cc493f4e.

--- Productos.Designer.cs
+++ Productos.Designer.cs
@@ -37,7 +37,7 @@
             txtNombreProducto = new TextBox();
             txtIdProducto = new TextBox();
             btnCargarProducto = new Button();
-            lblNombre = new Label();
+            label6 = new Label();
             label5 = new Label();
             label4 = new Label();
             label3 = new Label();
@@ -61,190 +61,173 @@
             // gridProductos
             // 
             gridProductos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            gridProductos.Location = new Point(403, 213);
-            gridProductos.Margin = new Padding(3, 4, 3, 4);
+            gridProductos.Location = new Point(353, 160);
             gridProductos.Name = "gridProductos";
-            gridProductos.RowHeadersWidth = 51;
-            gridProductos.Size = new Size(569, 307);
+            gridProductos.Size = new Size(498, 230);
             gridProductos.TabIndex = 29;
             // 
             // txtBuscarProductos
             // 
-            txtBuscarProductos.Location = new Point(401, 160);
-            txtBuscarProductos.Margin = new Padding(3, 4, 3, 4);
+            txtBuscarProductos.Location = new Point(351, 120);
             txtBuscarProductos.Name = "txtBuscarProductos";
-            txtBuscarProductos.Size = new Size(571, 27);
+            txtBuscarProductos.Size = new Size(500, 23);
             txtBuscarProductos.TabIndex = 28;
             // 
             // txtIdRubroProducto
             // 
-            txtIdRubroProducto.Location = new Point(191, 251);
-            txtIdRubroProducto.Margin = new Padding(3, 4, 3, 4);
+            txtIdRubroProducto.Location = new Point(167, 188);
             txtIdRubroProducto.Name = "txtIdRubroProducto";
-            txtIdRubroProducto.Size = new Size(114, 27);
+            txtIdRubroProducto.Size = new Size(100, 23);
             txtIdRubroProducto.TabIndex = 27;
             // 
             // txtCodigoBarraProducto
             // 
-            txtCodigoBarraProducto.Location = new Point(191, 209);
-            txtCodigoBarraProducto.Margin = new Padding(3, 4, 3, 4);
+            txtCodigoBarraProducto.Location = new Point(167, 157);
             txtCodigoBarraProducto.Name = "txtCodigoBarraProducto";
-            txtCodigoBarraProducto.Size = new Size(114, 27);
+            txtCodigoBarraProducto.Size = new Size(100, 23);
             txtCodigoBarraProducto.TabIndex = 26;
             // 
             // txtIdProveedorProducto
             // 
-            txtIdProveedorProducto.Location = new Point(191, 168);
-            txtIdProveedorProducto.Margin = new Padding(3, 4, 3, 4);
+            txtIdProveedorProducto.Location = new Point(167, 126);
             txtIdProveedorProducto.Name = "txtIdProveedorProducto";
-            txtIdProveedorProducto.Size = new Size(114, 27);
+            txtIdProveedorProducto.Size = new Size(100, 23);
             txtIdProveedorProducto.TabIndex = 25;
             // 
             // txtMarcaProducto
             // 
-            txtMarcaProducto.Location = new Point(191, 131);
-            txtMarcaProducto.Margin = new Padding(3, 4, 3, 4);
+            txtMarcaProducto.Location = new Point(167, 98);
             txtMarcaProducto.Name = "txtMarcaProducto";
-            txtMarcaProducto.Size = new Size(114, 27);
+            txtMarcaProducto.Size = new Size(100, 23);
             txtMarcaProducto.TabIndex = 24;
             // 
             // txtNombreProducto
             // 
-            txtNombreProducto.Location = new Point(191, 95);
-            txtNombreProducto.Margin = new Padding(3, 4, 3, 4);
+            txtNombreProducto.Location = new Point(167, 71);
             txtNombreProducto.Name = "txtNombreProducto";
-            txtNombreProducto.Size = new Size(114, 27);
+            txtNombreProducto.Size = new Size(100, 23);
             txtNombreProducto.TabIndex = 23;
             // 
             // txtIdProducto
             // 
             txtIdProducto.Enabled = false;
-            txtIdProducto.Location = new Point(191, 56);
-            txtIdProducto.Margin = new Padding(3, 4, 3, 4);
+            txtIdProducto.Location = new Point(167, 42);
             txtIdProducto.Name = "txtIdProducto";
-            txtIdProducto.Size = new Size(114, 27);
+            txtIdProducto.Size = new Size(100, 23);
             txtIdProducto.TabIndex = 22;
             // 
             // btnCargarProducto
             // 
-            btnCargarProducto.Location = new Point(105, 489);
-            btnCargarProducto.Margin = new Padding(3, 4, 3, 4);
+            btnCargarProducto.Location = new Point(92, 367);
             btnCargarProducto.Name = "btnCargarProducto";
-            btnCargarProducto.Size = new Size(86, 31);
+            btnCargarProducto.Size = new Size(75, 23);
             btnCargarProducto.TabIndex = 21;
             btnCargarProducto.Text = "Cargar";
             btnCargarProducto.UseVisualStyleBackColor = true;
             // 
-            // lblNombre
+            // label6
             // 
-            lblNombre.AutoSize = true;
-            lblNombre.Location = new Point(120, 105);
-            lblNombre.Name = "lblNombre";
-            lblNombre.Size = new Size(64, 20);
-            lblNombre.TabIndex = 20;
-            lblNombre.Text = "Nombre";
+            label6.AutoSize = true;
+            label6.Location = new Point(105, 79);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 15);
+            label6.TabIndex = 20;
+            label6.Text = "Nombre";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(120, 141);
+            label5.Location = new Point(105, 106);
             label5.Name = "label5";
-            label5.Size = new Size(50, 20);
+            label5.Size = new Size(40, 15);
             label5.TabIndex = 19;
             label5.Text = "Marca";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(120, 179);
+            label4.Location = new Point(105, 134);
             label4.Name = "label4";
-            label4.Size = new Size(77, 20);
+            label4.Size = new Size(61, 15);
             label4.TabIndex = 18;
             label4.Text = "Proveedor";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(114, 220);
+            label3.Location = new Point(100, 165);
             label3.Name = "label3";
-            label3.Size = new Size(78, 20);
+            label3.Size = new Size(62, 15);
             label3.TabIndex = 17;
             label3.Text = "Cod. Barra";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(120, 255);
+            label2.Location = new Point(105, 191);
             label2.Name = "label2";
-            label2.Size = new Size(49, 20);
+            label2.Size = new Size(39, 15);
             label2.TabIndex = 16;
             label2.Text = "Rubro";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(120, 60);
+            label1.Location = new Point(105, 45);
             label1.Name = "label1";
-            label1.Size = new Size(24, 20);
+            label1.Size = new Size(18, 15);
             label1.TabIndex = 15;
             label1.Text = "ID";
             // 
             // btnModificarProducto
             // 
-            btnModificarProducto.Location = new Point(198, 489);
-            btnModificarProducto.Margin = new Padding(3, 4, 3, 4);
+            btnModificarProducto.Location = new Point(173, 367);
             btnModificarProducto.Name = "btnModificarProducto";
-            btnModificarProducto.Size = new Size(86, 31);
+            btnModificarProducto.Size = new Size(75, 23);
             btnModificarProducto.TabIndex = 30;
             btnModificarProducto.Text = "Modificar";
             btnModificarProducto.UseVisualStyleBackColor = true;
             // 
             // txtStockActualProducto
             // 
-            txtStockActualProducto.Location = new Point(191, 289);
-            txtStockActualProducto.Margin = new Padding(3, 4, 3, 4);
+            txtStockActualProducto.Location = new Point(167, 217);
             txtStockActualProducto.Name = "txtStockActualProducto";
-            txtStockActualProducto.Size = new Size(114, 27);
+            txtStockActualProducto.Size = new Size(100, 23);
             txtStockActualProducto.TabIndex = 31;
             // 
             // txtStockMinimoProducto
             // 
-            txtStockMinimoProducto.Location = new Point(190, 328);
-            txtStockMinimoProducto.Margin = new Padding(3, 4, 3, 4);
+            txtStockMinimoProducto.Location = new Point(166, 246);
             txtStockMinimoProducto.Name = "txtStockMinimoProducto";
-            txtStockMinimoProducto.Size = new Size(114, 27);
+            txtStockMinimoProducto.Size = new Size(100, 23);
             txtStockMinimoProducto.TabIndex = 32;
             // 
             // txtPrecioDeCostoProducto
             // 
-            txtPrecioDeCostoProducto.Location = new Point(190, 367);
-            txtPrecioDeCostoProducto.Margin = new Padding(3, 4, 3, 4);
+            txtPrecioDeCostoProducto.Location = new Point(166, 275);
             txtPrecioDeCostoProducto.Name = "txtPrecioDeCostoProducto";
-            txtPrecioDeCostoProducto.Size = new Size(114, 27);
+            txtPrecioDeCostoProducto.Size = new Size(100, 23);
             txtPrecioDeCostoProducto.TabIndex = 33;
             // 
             // txtPorcentajeGananciaProducto
             // 
-            txtPorcentajeGananciaProducto.Location = new Point(191, 405);
-            txtPorcentajeGananciaProducto.Margin = new Padding(3, 4, 3, 4);
+            txtPorcentajeGananciaProducto.Location = new Point(167, 304);
             txtPorcentajeGananciaProducto.Name = "txtPorcentajeGananciaProducto";
-            txtPorcentajeGananciaProducto.Size = new Size(114, 27);
+            txtPorcentajeGananciaProducto.Size = new Size(100, 23);
             txtPorcentajeGananciaProducto.TabIndex = 34;
             // 
             // txtPrecioDeVentaProducto
             // 
-            txtPrecioDeVentaProducto.Location = new Point(191, 444);
-            txtPrecioDeVentaProducto.Margin = new Padding(3, 4, 3, 4);
+            txtPrecioDeVentaProducto.Location = new Point(167, 333);
             txtPrecioDeVentaProducto.Name = "txtPrecioDeVentaProducto";
-            txtPrecioDeVentaProducto.Size = new Size(114, 27);
+            txtPrecioDeVentaProducto.Size = new Size(100, 23);
             txtPrecioDeVentaProducto.TabIndex = 35;
             // 
             // btnEliminarProducto
             // 
-            btnEliminarProducto.Location = new Point(290, 489);
-            btnEliminarProducto.Margin = new Padding(3, 4, 3, 4);
+            btnEliminarProducto.Location = new Point(254, 367);
             btnEliminarProducto.Name = "btnEliminarProducto";
-            btnEliminarProducto.Size = new Size(86, 31);
+            btnEliminarProducto.Size = new Size(75, 23);
             btnEliminarProducto.TabIndex = 36;
             btnEliminarProducto.Text = "Eliminar";
             btnEliminarProducto.UseVisualStyleBackColor = true;
@@ -252,53 +235,53 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(102, 300);
+            label7.Location = new Point(89, 225);
             label7.Name = "label7";
-            label7.Size = new Size(91, 20);
+            label7.Size = new Size(73, 15);
             label7.TabIndex = 37;
             label7.Text = "Stock Actual";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(93, 332);
+            label8.Location = new Point(81, 249);
             label8.Name = "label8";
-            label8.Size = new Size(100, 20);
+            label8.Size = new Size(81, 15);
             label8.TabIndex = 38;
             label8.Text = "Stock Minimo";
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(80, 371);
+            label9.Location = new Point(70, 278);
             label9.Name = "label9";
-            label9.Size = new Size(113, 20);
+            label9.Size = new Size(90, 15);
             label9.TabIndex = 39;
             label9.Text = "Precio de Costo";
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(47, 416);
+            label10.Location = new Point(41, 312);
             label10.Name = "label10";
-            label10.Size = new Size(143, 20);
+            label10.Size = new Size(115, 15);
             label10.TabIndex = 40;
             label10.Text = "Porcentaje Ganancia";
             // 
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(78, 455);
+            label11.Location = new Point(68, 341);
             label11.Name = "label11";
-            label11.Size = new Size(112, 20);
+            label11.Size = new Size(88, 15);
             label11.TabIndex = 41;
             label11.Text = "Precio de Venta";
             // 
             // Productos
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(800, 450);
             Controls.Add(label11);
             Controls.Add(label10);
             Controls.Add(label9);
@@ -320,13 +303,12 @@
             Controls.Add(txtNombreProducto);
             Controls.Add(txtIdProducto);
             Controls.Add(btnCargarProducto);
-            Controls.Add(lblNombre);
+            Controls.Add(label6);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Productos";
             Text = "Productos";
             ((System.ComponentModel.ISupportInitialize)gridProductos).EndInit();
@@ -345,7 +327,7 @@
         private TextBox txtNombreProducto;
         private TextBox txtIdProducto;
         private Button btnCargarProducto;
-        private Label lblNombre;
+        private Label label6;
         private Label label5;
         private Label label4;
         private Label label3;
