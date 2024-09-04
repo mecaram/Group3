d9d6b143eb7e9e5f7a2574b4d2dc3dbbcd4fa2c5
RamiroTaverna <ramirotaverna18@gmail.com>
2024-09-04 20:16:55 -0300

    FORMULARIO AVANZADO

--- Clientes.Designer.cs
+++ Clientes.Designer.cs
@@ -39,178 +39,172 @@
             txtNombreCliente = new TextBox();
             txtApellidoCliente = new TextBox();
             txtDniCliente = new TextBox();
-            txtCiudadCliente = new TextBox();
             txtDireccionCliente = new TextBox();
             txtBuscarCliente = new TextBox();
             gridCliente = new DataGridView();
             btnModificarCliente = new Button();
             btnEliminarCliente = new Button();
+            cboCiudadCliente = new ComboBox();
             ((System.ComponentModel.ISupportInitialize)gridCliente).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(15, 41);
+            label1.Location = new Point(13, 31);
             label1.Name = "label1";
-            label1.Size = new Size(24, 20);
+            label1.Size = new Size(18, 15);
             label1.TabIndex = 0;
             label1.Text = "ID";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(15, 243);
+            label2.Location = new Point(13, 182);
             label2.Name = "label2";
-            label2.Size = new Size(72, 20);
+            label2.Size = new Size(57, 15);
             label2.TabIndex = 1;
             label2.Text = "Direccion";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(15, 197);
+            label3.Location = new Point(13, 148);
             label3.Name = "label3";
-            label3.Size = new Size(56, 20);
+            label3.Size = new Size(45, 15);
             label3.TabIndex = 2;
             label3.Text = "Ciudad";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(15, 156);
+            label4.Location = new Point(13, 117);
             label4.Name = "label4";
-            label4.Size = new Size(35, 20);
+            label4.Size = new Size(27, 15);
             label4.TabIndex = 3;
             label4.Text = "DNI";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(15, 117);
+            label5.Location = new Point(13, 88);
             label5.Name = "label5";
-            label5.Size = new Size(66, 20);
+            label5.Size = new Size(51, 15);
             label5.TabIndex = 4;
             label5.Text = "Apellido";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(14, 81);
+            label6.Location = new Point(12, 61);
             label6.Name = "label6";
-            label6.Size = new Size(64, 20);
+            label6.Size = new Size(51, 15);
             label6.TabIndex = 5;
             label6.Text = "Nombre";
             // 
             // btnCargarCliente
             // 
-            btnCargarCliente.Location = new Point(13, 312);
-            btnCargarCliente.Margin = new Padding(3, 4, 3, 4);
+            btnCargarCliente.Location = new Point(11, 234);
             btnCargarCliente.Name = "btnCargarCliente";
-            btnCargarCliente.Size = new Size(86, 31);
+            btnCargarCliente.Size = new Size(75, 23);
             btnCargarCliente.TabIndex = 6;
             btnCargarCliente.Text = "Cargar";
             btnCargarCliente.UseVisualStyleBackColor = true;
+            btnCargarCliente.Click += btnCargarCliente_Click;
             // 
             // txtIdCliente
             // 
-            txtIdCliente.Location = new Point(85, 37);
-            txtIdCliente.Margin = new Padding(3, 4, 3, 4);
+            txtIdCliente.Location = new Point(74, 28);
             txtIdCliente.Name = "txtIdCliente";
-            txtIdCliente.Size = new Size(114, 27);
+            txtIdCliente.Size = new Size(100, 23);
             txtIdCliente.TabIndex = 7;
             // 
             // txtNombreCliente
             // 
-            txtNombreCliente.Location = new Point(85, 76);
-            txtNombreCliente.Margin = new Padding(3, 4, 3, 4);
+            txtNombreCliente.Location = new Point(74, 57);
             txtNombreCliente.Name = "txtNombreCliente";
-            txtNombreCliente.Size = new Size(114, 27);
+            txtNombreCliente.Size = new Size(100, 23);
             txtNombreCliente.TabIndex = 8;
             // 
             // txtApellidoCliente
             // 
-            txtApellidoCliente.Location = new Point(85, 112);
-            txtApellidoCliente.Margin = new Padding(3, 4, 3, 4);
+            txtApellidoCliente.Location = new Point(74, 84);
             txtApellidoCliente.Name = "txtApellidoCliente";
-            txtApellidoCliente.Size = new Size(114, 27);
+            txtApellidoCliente.Size = new Size(100, 23);
             txtApellidoCliente.TabIndex = 9;
             // 
             // txtDniCliente
             // 
-            txtDniCliente.Location = new Point(85, 149);
-            txtDniCliente.Margin = new Padding(3, 4, 3, 4);
+            txtDniCliente.Location = new Point(74, 112);
             txtDniCliente.Name = "txtDniCliente";
-            txtDniCliente.Size = new Size(114, 27);
+            txtDniCliente.Size = new Size(100, 23);
             txtDniCliente.TabIndex = 10;
-            // 
-            // txtCiudadCliente
-            // 
-            txtCiudadCliente.Location = new Point(85, 191);
-            txtCiudadCliente.Margin = new Padding(3, 4, 3, 4);
-            txtCiudadCliente.Name = "txtCiudadCliente";
-            txtCiudadCliente.Size = new Size(114, 27);
-            txtCiudadCliente.TabIndex = 11;
             // 
             // txtDireccionCliente
             // 
-            txtDireccionCliente.Location = new Point(85, 241);
-            txtDireccionCliente.Margin = new Padding(3, 4, 3, 4);
+            txtDireccionCliente.Location = new Point(74, 181);
             txtDireccionCliente.Name = "txtDireccionCliente";
-            txtDireccionCliente.Size = new Size(114, 27);
+            txtDireccionCliente.Size = new Size(100, 23);
             txtDireccionCliente.TabIndex = 12;
             // 
             // txtBuscarCliente
             // 
-            txtBuscarCliente.Location = new Point(289, 12);
-            txtBuscarCliente.Margin = new Padding(3, 4, 3, 4);
+            txtBuscarCliente.Location = new Point(253, 9);
             txtBuscarCliente.Name = "txtBuscarCliente";
-            txtBuscarCliente.Size = new Size(571, 27);
+            txtBuscarCliente.Size = new Size(500, 23);
             txtBuscarCliente.TabIndex = 13;
             // 
             // gridCliente
             // 
             gridCliente.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            gridCliente.Location = new Point(291, 65);
-            gridCliente.Margin = new Padding(3, 4, 3, 4);
+            gridCliente.Location = new Point(255, 49);
             gridCliente.Name = "gridCliente";
             gridCliente.RowHeadersWidth = 51;
             gridCliente.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            gridCliente.Size = new Size(569, 307);
+            gridCliente.Size = new Size(498, 230);
             gridCliente.TabIndex = 14;
             gridCliente.SelectionChanged += gridCliente_SelectionChanged;
             // 
             // btnModificarCliente
             // 
-            btnModificarCliente.Location = new Point(106, 313);
-            btnModificarCliente.Margin = new Padding(3, 4, 3, 4);
+            btnModificarCliente.Location = new Point(93, 235);
             btnModificarCliente.Name = "btnModificarCliente";
-            btnModificarCliente.Size = new Size(86, 31);
+            btnModificarCliente.Size = new Size(75, 23);
             btnModificarCliente.TabIndex = 15;
             btnModificarCliente.Text = "Modificar";
             btnModificarCliente.UseVisualStyleBackColor = true;
+            btnModificarCliente.Click += btnModificarCliente_Click;
             // 
             // btnEliminarCliente
             // 
-            btnEliminarCliente.Location = new Point(199, 313);
-            btnEliminarCliente.Margin = new Padding(3, 4, 3, 4);
+            btnEliminarCliente.Location = new Point(174, 235);
             btnEliminarCliente.Name = "btnEliminarCliente";
-            btnEliminarCliente.Size = new Size(86, 31);
+            btnEliminarCliente.Size = new Size(75, 23);
             btnEliminarCliente.TabIndex = 16;
             btnEliminarCliente.Text = "Eliminar";
             btnEliminarCliente.UseVisualStyleBackColor = true;
+            btnEliminarCliente.Click += btnEliminarCliente_Click;
+            // 
+            // cboCiudadCliente
+            // 
+            cboCiudadCliente.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboCiudadCliente.FormattingEnabled = true;
+            cboCiudadCliente.Location = new Point(76, 146);
+            cboCiudadCliente.Name = "cboCiudadCliente";
+            cboCiudadCliente.Size = new Size(98, 23);
+            cboCiudadCliente.TabIndex = 17;
             // 
             // Clientes
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(800, 450);
+            Controls.Add(cboCiudadCliente);
             Controls.Add(btnEliminarCliente);
             Controls.Add(btnModificarCliente);
             Controls.Add(gridCliente);
             Controls.Add(txtBuscarCliente);
             Controls.Add(txtDireccionCliente);
-            Controls.Add(txtCiudadCliente);
             Controls.Add(txtDniCliente);
             Controls.Add(txtApellidoCliente);
             Controls.Add(txtNombreCliente);
@@ -222,7 +216,6 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Clientes";
             Text = "Clientes";
             Load += Clientes_Load;
@@ -244,11 +237,11 @@
         private TextBox txtNombreCliente;
         private TextBox txtApellidoCliente;
         private TextBox txtDniCliente;
-        private TextBox txtCiudadCliente;
         private TextBox txtDireccionCliente;
         private TextBox txtBuscarCliente;
         private DataGridView gridCliente;
         private Button btnModificarCliente;
         private Button btnEliminarCliente;
+        private ComboBox cboCiudadCliente;
     }
 }
--- Clientes.cs
+++ Clientes.cs
@@ -1,13 +1,19 @@
 ﻿using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+using MySqlX.XDevAPI;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+//BUSCADOR CON LIKE
+//SELECT * FROM supermercadodb.clientes Where concat(nombre,' ',apellido) like '%juan lopez%';
 
 namespace Gestion
 {
@@ -21,10 +27,9 @@ namespace Gestion
         private void Clientes_Load(object sender, EventArgs e)
         {
             // CONEXION BASE DE DATOS
+            MySqlConnection conexion = new MySqlConnection("Data Source= localhost; Initial Catalog = supermercadodb; Uid = root; Pwd = 1337");
 
-            MySqlConnection conexion = new MySqlConnection("Data Source= localhost; Initial Catalog = gestion; Uid = root; Pwd = 123456789");
-
-            MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM clientes", conexion);
+            MySqlDataAdapter da = new MySqlDataAdapter("Select * From clientes", conexion);
 
             // CARGAR DATOS EN LA GRILLA
 
@@ -36,17 +41,89 @@ namespace Gestion
             {
                 gridCliente.DataSource = dt;
             }
+            else
+            {
+                gridCliente.DataSource = null;
+            }
+
+            MySqlDataAdapter daCiudades = new MySqlDataAdapter("Select * From ciudades", conexion);
+
+            // CARGAR DATOS EN LA GRILLA
+
+            DataTable dtCiudades = new DataTable();
+
+            registros = daCiudades.Fill(dtCiudades);
+
+            if (registros > 0)
+            {
+                cboCiudadCliente.DataSource = dtCiudades;
+                cboCiudadCliente.DisplayMember = "ciudad";
+                cboCiudadCliente.ValueMember = "id_ciudad";
+            }
+            else
+            {
+                cboCiudadCliente.DataSource = null;
+            }
         }
 
-        private void gridCliente_SelectionChanged(object sender, EventArgs e)
+        public void gridCliente_SelectionChanged(object sender, EventArgs e)
         {
             // CARGAR DATOS DE LA GRILLA EN LOS TEXTBOX
             txtIdCliente.Text = gridCliente.CurrentRow.Cells["id_cliente"].Value.ToString();
             txtNombreCliente.Text = gridCliente.CurrentRow.Cells["nombre"].Value.ToString();
             txtApellidoCliente.Text = gridCliente.CurrentRow.Cells["apellido"].Value.ToString();
             txtDniCliente.Text = gridCliente.CurrentRow.Cells["dni"].Value.ToString();
-            txtCiudadCliente.Text = gridCliente.CurrentRow.Cells["id_ciudad"].Value.ToString();
+            cboCiudadCliente.SelectedValue = gridCliente.CurrentRow.Cells["id_ciudad"].Value.ToString();
             txtDireccionCliente.Text = gridCliente.CurrentRow.Cells["direccion"].Value.ToString();
         }
+
+        private void btnModificarCliente_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminarCliente_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Desea realmente eliminar este Registro ?", "Solicitud del Sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == System.Windows.Forms.DialogResult.Yes)
+            {
+                MySqlConnection conexion = new MySqlConnection("Data Source= localhost; Initial Catalog = supermercadodb; Uid = root; Pwd = 1337");
+                conexion.Open();
+
+                string sentencia = string.Format("Delete From clientes Where id_cliente = {0}", txtIdCliente.Text.Trim());
+                var cmdEliminar = new MySqlCommand(sentencia, conexion);
+                cmdEliminar.ExecuteNonQuery();
+
+                conexion.Close();
+                Clientes_Load(sender, e);
+
+                txtIdCliente.Clear();
+                txtNombreCliente.Text = "";
+                txtApellidoCliente.Text = "";
+                txtDniCliente.Text = "";
+                cboCiudadCliente.SelectedValue = 0;
+                txtDireccionCliente.Text = "";
+            }
+        }
+
+        private void btnCargarCliente_Click(object sender, EventArgs e)
+        {
+        MySqlConnection conexion = new MySqlConnection("Data Source= localhost; Initial Catalog = supermercadodb; Uid = root; Pwd = 1337");
+        conexion.Open();
+
+        string nombre = txtNombreCliente.Text;
+        string apellido = txtApellidoCliente.Text;
+        string dni = txtDniCliente.Text;
+        int ciudad = Convert.ToInt32(cboCiudadCliente.SelectedValue);
+        string direccion = txtDireccionCliente.Text;
+
+        string cargaCliente = string.Format("INSERT INTO `supermercadodb`.`clientes` (`nombre`, `apellido`, `dni`, `id_ciudad`, `direccion`) VALUES ('{0}', '{1}', '{2}', {3}, '{4}');", nombre, apellido, dni, ciudad, direccion);
+
+        var cmdCargar = new MySqlCommand(cargaCliente, conexion);
+        cmdCargar.ExecuteNonQuery();
+
+        conexion.Close();
+        Clientes_Load(sender, e);
+}
+
     }
 }
