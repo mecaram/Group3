c7a02d2f6a2a6fa1c179083280ee4636aef47d34
Thiago Lozano <thiagolozano47@gmail.com>
2024-09-25 19:54:08 -0300

    Actualizacion de Productos

--- Productos.Designer.cs
+++ Productos.Designer.cs
@@ -30,16 +30,15 @@
         {
             gridProductos = new DataGridView();
             txtBuscarProductos = new TextBox();
-            txtIdRubroProducto = new TextBox();
             txtCodigoBarraProducto = new TextBox();
             txtNombreProducto = new TextBox();
             txtIdProducto = new TextBox();
             btnCargarProducto = new Button();
-            label6 = new Label();
-            label5 = new Label();
-            label4 = new Label();
-            label3 = new Label();
-            label2 = new Label();
+            lblNombre = new Label();
+            lblMarca = new Label();
+            lblProveedor = new Label();
+            lblCodBarra = new Label();
+            lblRubro = new Label();
             label1 = new Label();
             btnModificarProducto = new Button();
             txtStockActualProducto = new TextBox();
@@ -68,55 +67,40 @@
             gridProductos.RowHeadersWidth = 51;
             gridProductos.Size = new Size(569, 307);
             gridProductos.TabIndex = 29;
+            
             // 
             // txtBuscarProductos
             // 
-            txtBuscarProductos.Location = new Point(351, 120);
+            txtBuscarProductos.Location = new Point(373, 75);
+            txtBuscarProductos.Margin = new Padding(3, 4, 3, 4);
             txtBuscarProductos.Name = "txtBuscarProductos";
-            txtBuscarProductos.Size = new Size(500, 23);
+            txtBuscarProductos.Size = new Size(571, 27);
             txtBuscarProductos.TabIndex = 28;
-            // 
-            // txtIdRubroProducto
-            // 
-            txtIdRubroProducto.Location = new Point(167, 188);
-            txtIdRubroProducto.Name = "txtIdRubroProducto";
-            txtIdRubroProducto.Size = new Size(100, 23);
-            txtIdRubroProducto.TabIndex = 27;
+     
             // 
             // txtCodigoBarraProducto
             // 
             txtCodigoBarraProducto.Location = new Point(198, 213);
             txtCodigoBarraProducto.Margin = new Padding(3, 4, 3, 4);
             txtCodigoBarraProducto.Name = "txtCodigoBarraProducto";
-            txtCodigoBarraProducto.Size = new Size(100, 23);
+            txtCodigoBarraProducto.Size = new Size(114, 27);
             txtCodigoBarraProducto.TabIndex = 26;
-            // 
-            // txtIdProveedorProducto
-            // 
-            txtIdProveedorProducto.Location = new Point(167, 126);
-            txtIdProveedorProducto.Name = "txtIdProveedorProducto";
-            txtIdProveedorProducto.Size = new Size(100, 23);
-            txtIdProveedorProducto.TabIndex = 25;
-            // 
-            // txtMarcaProducto
-            // 
-            txtMarcaProducto.Location = new Point(167, 98);
-            txtMarcaProducto.Name = "txtMarcaProducto";
-            txtMarcaProducto.Size = new Size(100, 23);
-            txtMarcaProducto.TabIndex = 24;
+           
             // 
             // txtNombreProducto
             // 
             txtNombreProducto.Location = new Point(199, 110);
             txtNombreProducto.Margin = new Padding(3, 4, 3, 4);
             txtNombreProducto.Name = "txtNombreProducto";
-            txtNombreProducto.Size = new Size(100, 23);
+            txtNombreProducto.Size = new Size(114, 27);
             txtNombreProducto.TabIndex = 23;
+           
             // 
             // txtIdProducto
             // 
             txtIdProducto.Enabled = false;
-            txtIdProducto.Location = new Point(167, 42);
+            txtIdProducto.Location = new Point(199, 75);
+            txtIdProducto.Margin = new Padding(3, 4, 3, 4);
             txtIdProducto.Name = "txtIdProducto";
             txtIdProducto.Size = new Size(114, 27);
             txtIdProducto.TabIndex = 22;
@@ -124,12 +108,14 @@
             // 
             // btnCargarProducto
             // 
-            btnCargarProducto.Location = new Point(92, 367);
+            btnCargarProducto.Location = new Point(80, 513);
+            btnCargarProducto.Margin = new Padding(3, 4, 3, 4);
             btnCargarProducto.Name = "btnCargarProducto";
-            btnCargarProducto.Size = new Size(75, 23);
+            btnCargarProducto.Size = new Size(86, 31);
             btnCargarProducto.TabIndex = 21;
             btnCargarProducto.Text = "Cargar";
             btnCargarProducto.UseVisualStyleBackColor = true;
+          
             // 
             // lblNombre
             // 
@@ -140,25 +126,25 @@
             lblNombre.TabIndex = 20;
             lblNombre.Text = "Nombre";
             // 
-            // label5
+            // lblMarca
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(105, 106);
-            label5.Name = "label5";
-            label5.Size = new Size(40, 15);
-            label5.TabIndex = 19;
-            label5.Text = "Marca";
+            lblMarca.AutoSize = true;
+            lblMarca.Location = new Point(120, 141);
+            lblMarca.Name = "lblMarca";
+            lblMarca.Size = new Size(50, 20);
+            lblMarca.TabIndex = 19;
+            lblMarca.Text = "Marca";
             // 
-            // label4
+            // lblProveedor
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(105, 134);
-            label4.Name = "label4";
-            label4.Size = new Size(61, 15);
-            label4.TabIndex = 18;
-            label4.Text = "Proveedor";
+            lblProveedor.AutoSize = true;
+            lblProveedor.Location = new Point(105, 181);
+            lblProveedor.Name = "lblProveedor";
+            lblProveedor.Size = new Size(77, 20);
+            lblProveedor.TabIndex = 18;
+            lblProveedor.Text = "Proveedor";
             // 
-            // label3
+            // lblCodBarra
             // 
             lblCodBarra.AutoSize = true;
             lblCodBarra.Location = new Point(105, 216);
@@ -167,21 +153,21 @@
             lblCodBarra.TabIndex = 17;
             lblCodBarra.Text = "Cod. Barra:";
             // 
-            // label2
+            // lblRubro
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(105, 191);
-            label2.Name = "label2";
-            label2.Size = new Size(39, 15);
-            label2.TabIndex = 16;
-            label2.Text = "Rubro";
+            lblRubro.AutoSize = true;
+            lblRubro.Location = new Point(120, 255);
+            lblRubro.Name = "lblRubro";
+            lblRubro.Size = new Size(52, 20);
+            lblRubro.TabIndex = 16;
+            lblRubro.Text = "Rubro:";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(105, 45);
+            label1.Location = new Point(120, 60);
             label1.Name = "label1";
-            label1.Size = new Size(18, 15);
+            label1.Size = new Size(24, 20);
             label1.TabIndex = 15;
             label1.Text = "ID";
             // 
@@ -194,103 +180,146 @@
             btnModificarProducto.TabIndex = 30;
             btnModificarProducto.Text = "Modificar";
             btnModificarProducto.UseVisualStyleBackColor = true;
+          
             // 
             // txtStockActualProducto
             // 
-            txtStockActualProducto.Location = new Point(167, 217);
+            txtStockActualProducto.Location = new Point(199, 282);
+            txtStockActualProducto.Margin = new Padding(3, 4, 3, 4);
             txtStockActualProducto.Name = "txtStockActualProducto";
-            txtStockActualProducto.Size = new Size(100, 23);
+            txtStockActualProducto.Size = new Size(114, 27);
             txtStockActualProducto.TabIndex = 31;
+           
             // 
             // txtStockMinimoProducto
             // 
-            txtStockMinimoProducto.Location = new Point(166, 246);
+            txtStockMinimoProducto.Location = new Point(198, 317);
+            txtStockMinimoProducto.Margin = new Padding(3, 4, 3, 4);
             txtStockMinimoProducto.Name = "txtStockMinimoProducto";
-            txtStockMinimoProducto.Size = new Size(100, 23);
+            txtStockMinimoProducto.Size = new Size(114, 27);
             txtStockMinimoProducto.TabIndex = 32;
+          
             // 
             // txtPrecioDeCostoProducto
             // 
-            txtPrecioDeCostoProducto.Location = new Point(166, 275);
+            txtPrecioDeCostoProducto.Location = new Point(199, 352);
+            txtPrecioDeCostoProducto.Margin = new Padding(3, 4, 3, 4);
             txtPrecioDeCostoProducto.Name = "txtPrecioDeCostoProducto";
-            txtPrecioDeCostoProducto.Size = new Size(100, 23);
+            txtPrecioDeCostoProducto.Size = new Size(114, 27);
             txtPrecioDeCostoProducto.TabIndex = 33;
+        
             // 
             // txtPorcentajeGananciaProducto
             // 
-            txtPorcentajeGananciaProducto.Location = new Point(167, 304);
+            txtPorcentajeGananciaProducto.Location = new Point(198, 387);
+            txtPorcentajeGananciaProducto.Margin = new Padding(3, 4, 3, 4);
             txtPorcentajeGananciaProducto.Name = "txtPorcentajeGananciaProducto";
-            txtPorcentajeGananciaProducto.Size = new Size(100, 23);
+            txtPorcentajeGananciaProducto.Size = new Size(114, 27);
             txtPorcentajeGananciaProducto.TabIndex = 34;
+      
             // 
             // txtPrecioDeVentaProducto
             // 
-            txtPrecioDeVentaProducto.Location = new Point(167, 333);
+            txtPrecioDeVentaProducto.Location = new Point(196, 422);
+            txtPrecioDeVentaProducto.Margin = new Padding(3, 4, 3, 4);
             txtPrecioDeVentaProducto.Name = "txtPrecioDeVentaProducto";
-            txtPrecioDeVentaProducto.Size = new Size(100, 23);
+            txtPrecioDeVentaProducto.Size = new Size(114, 27);
             txtPrecioDeVentaProducto.TabIndex = 35;
+          
             // 
             // btnEliminarProducto
             // 
-            btnEliminarProducto.Location = new Point(254, 367);
+            btnEliminarProducto.Location = new Point(290, 489);
+            btnEliminarProducto.Margin = new Padding(3, 4, 3, 4);
             btnEliminarProducto.Name = "btnEliminarProducto";
-            btnEliminarProducto.Size = new Size(75, 23);
+            btnEliminarProducto.Size = new Size(86, 31);
             btnEliminarProducto.TabIndex = 36;
             btnEliminarProducto.Text = "Eliminar";
             btnEliminarProducto.UseVisualStyleBackColor = true;
+         
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(89, 225);
+            label7.Location = new Point(102, 300);
             label7.Name = "label7";
-            label7.Size = new Size(73, 15);
+            label7.Size = new Size(91, 20);
             label7.TabIndex = 37;
             label7.Text = "Stock Actual";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(81, 249);
+            label8.Location = new Point(93, 332);
             label8.Name = "label8";
-            label8.Size = new Size(81, 15);
+            label8.Size = new Size(100, 20);
             label8.TabIndex = 38;
             label8.Text = "Stock Minimo";
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(70, 278);
+            label9.Location = new Point(80, 371);
             label9.Name = "label9";
-            label9.Size = new Size(90, 15);
+            label9.Size = new Size(113, 20);
             label9.TabIndex = 39;
             label9.Text = "Precio de Costo";
             // 
-            // label10
+            // lblPorcentajeGanancia
             // 
-            label10.AutoSize = true;
-            label10.Location = new Point(41, 312);
-            label10.Name = "label10";
-            label10.Size = new Size(115, 15);
-            label10.TabIndex = 40;
-            label10.Text = "Porcentaje Ganancia";
+            lblPorcentajeGanancia.AutoSize = true;
+            lblPorcentajeGanancia.Location = new Point(47, 416);
+            lblPorcentajeGanancia.Name = "lblPorcentajeGanancia";
+            lblPorcentajeGanancia.Size = new Size(143, 20);
+            lblPorcentajeGanancia.TabIndex = 40;
+            lblPorcentajeGanancia.Text = "Porcentaje Ganancia";
             // 
-            // label11
+            // lblPreciodeVenta
             // 
-            label11.AutoSize = true;
-            label11.Location = new Point(68, 341);
-            label11.Name = "label11";
-            label11.Size = new Size(88, 15);
-            label11.TabIndex = 41;
-            label11.Text = "Precio de Venta";
+            lblPreciodeVenta.AutoSize = true;
+            lblPreciodeVenta.Location = new Point(78, 455);
+            lblPreciodeVenta.Name = "lblPreciodeVenta";
+            lblPreciodeVenta.Size = new Size(112, 20);
+            lblPreciodeVenta.TabIndex = 41;
+            lblPreciodeVenta.Text = "Precio de Venta";
+            // 
+            // cboMarca
+            // 
+            cboMarca.FormattingEnabled = true;
+            cboMarca.Location = new Point(198, 144);
+            cboMarca.Name = "cboMarca";
+            cboMarca.Size = new Size(114, 28);
+            cboMarca.TabIndex = 42;
+           
+            // 
+            // cboProveedor
+            // 
+            cboProveedor.FormattingEnabled = true;
+            cboProveedor.Location = new Point(199, 178);
+            cboProveedor.Name = "cboProveedor";
+            cboProveedor.Size = new Size(114, 28);
+            cboProveedor.TabIndex = 43;
+      
+            // 
+            // cboRubro
+            // 
+            cboRubro.FormattingEnabled = true;
+            cboRubro.Location = new Point(198, 247);
+            cboRubro.Name = "cboRubro";
+            cboRubro.Size = new Size(114, 28);
+            cboRubro.TabIndex = 44;
+           
             // 
             // Productos
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label11);
-            Controls.Add(label10);
+            ClientSize = new Size(1086, 600);
+            Controls.Add(cboRubro);
+            Controls.Add(cboProveedor);
+            Controls.Add(cboMarca);
+            Controls.Add(lblPreciodeVenta);
+            Controls.Add(lblPorcentajeGanancia);
             Controls.Add(label9);
             Controls.Add(label8);
             Controls.Add(label7);
@@ -303,21 +332,20 @@
             Controls.Add(btnModificarProducto);
             Controls.Add(gridProductos);
             Controls.Add(txtBuscarProductos);
-            Controls.Add(txtIdRubroProducto);
             Controls.Add(txtCodigoBarraProducto);
-            Controls.Add(txtIdProveedorProducto);
-            Controls.Add(txtMarcaProducto);
             Controls.Add(txtNombreProducto);
             Controls.Add(txtIdProducto);
             Controls.Add(btnCargarProducto);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
+            Controls.Add(lblNombre);
+            Controls.Add(lblMarca);
+            Controls.Add(lblProveedor);
+            Controls.Add(lblCodBarra);
+            Controls.Add(lblRubro);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Productos";
             Text = "Productos";
+            Load += Productos_Load;
             ((System.ComponentModel.ISupportInitialize)gridProductos).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -327,10 +355,7 @@
 
         private DataGridView gridProductos;
         private TextBox txtBuscarProductos;
-        private TextBox txtIdRubroProducto;
         private TextBox txtCodigoBarraProducto;
-        private TextBox txtIdProveedorProducto;
-        private TextBox txtMarcaProducto;
         private TextBox txtNombreProducto;
         private TextBox txtIdProducto;
         private Button btnCargarProducto;
@@ -350,7 +375,10 @@
         private Label label7;
         private Label label8;
         private Label label9;
-        private Label label10;
-        private Label label11;
+        private Label lblPorcentajeGanancia;
+        private Label lblPreciodeVenta;
+        private ComboBox cboMarca;
+        private ComboBox cboProveedor;
+        private ComboBox cboRubro;
     }
 }
--- Productos.cs
+++ Productos.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
 using System.Data;
 using System.Windows.Forms;
 
@@ -8,9 +8,214 @@ namespace Gestion
 {
     public partial class Productos : Form
     {
+        string conexionBD = ConfigurationManager.ConnectionStrings["conexionBD"].ToString();
+
         public Productos()
         {
             InitializeComponent();
+        }
+
+        private void Productos_Load(object sender, EventArgs e)
+        {
+            // Deshabilitar edición en la grilla y selección múltiple
+            gridProductos.ReadOnly = true;
+            gridProductos.MultiSelect = false;
+
+            // Deshabilitar el campo de ID
+            txtIdProducto.Enabled = false;
+
+            // Cargar datos iniciales
+            CargarCombos();
+            CargarProductos();
+        }
+
+        // Método para cargar los datos en la grilla de productos
+        private void CargarProductos()
+        {
+            using (MySqlConnection conexion = new MySqlConnection(conexionBD))
+            {
+                string query = @"SELECT p.id_producto, p.nombre, p.stock_actual, p.stock_minimo, 
+                                        p.precio_de_costo, p.precio_ganancia, p.precio_venta,
+                                        m.nombre AS marca, r.nombre AS rubro, pr.razon_social AS proveedor
+                                 FROM productos p
+                                 JOIN marcas m ON p.id_marca = m.id_marca
+                                 JOIN rubros r ON p.id_rubro = r.id_rubro
+                                 JOIN proveedores pr ON p.id_proveedor = pr.id_proveedor";
+
+                MySqlDataAdapter daProductos = new MySqlDataAdapter(query, conexion);
+                DataTable dtProductos = new DataTable();
+                daProductos.Fill(dtProductos);
+
+                gridProductos.DataSource = dtProductos;
+            }
+        }
+
+        // Método para cargar los datos de los combos
+        private void CargarCombos()
+        {
+            using (MySqlConnection conexion = new MySqlConnection(conexionBD))
+            {
+                // Cargar Combo de Marcas
+                MySqlDataAdapter daMarcas = new MySqlDataAdapter("SELECT id_marca, nombre FROM marcas", conexion);
+                DataTable dtMarcas = new DataTable();
+                daMarcas.Fill(dtMarcas);
+                cboMarca.DataSource = dtMarcas;
+                cboMarca.DisplayMember = "nombre";
+                cboMarca.ValueMember = "id_marca";
+
+                // Cargar Combo de Rubros
+                MySqlDataAdapter daRubros = new MySqlDataAdapter("SELECT id_rubro, nombre FROM rubros", conexion);
+                DataTable dtRubros = new DataTable();
+                daRubros.Fill(dtRubros);
+                cboRubro.DataSource = dtRubros;
+                cboRubro.DisplayMember = "nombre";
+                cboRubro.ValueMember = "id_rubro";
+
+                // Cargar Combo de Proveedores
+                MySqlDataAdapter daProveedores = new MySqlDataAdapter("SELECT id_proveedor, razon_social FROM proveedores", conexion);
+                DataTable dtProveedores = new DataTable();
+                daProveedores.Fill(dtProveedores);
+                cboProveedor.DataSource = dtProveedores;
+                cboProveedor.DisplayMember = "razon_social";
+                cboProveedor.ValueMember = "id_proveedor";
+            }
+        }
+
+        // Evento para seleccionar un producto en la grilla
+        private void gridProductos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (gridProductos.SelectedRows.Count > 0 && gridProductos.CurrentRow != null)
+            {
+                // Cargar los datos en los TextBox y ComboBox
+                txtIdProducto.Text = gridProductos.CurrentRow.Cells["id_producto"].Value.ToString();
+                txtNombreProducto.Text = gridProductos.CurrentRow.Cells["nombre"].Value.ToString();
+                txtStockActualProducto.Text = gridProductos.CurrentRow.Cells["stock_actual"].Value.ToString();
+                txtStockMinimoProducto.Text = gridProductos.CurrentRow.Cells["stock_minimo"].Value.ToString();
+                txtPrecioDeCostoProducto.Text = gridProductos.CurrentRow.Cells["precio_costo"].Value.ToString();
+                txtPorcentajeGananciaProducto.Text = gridProductos.CurrentRow.Cells["precio_ganancia"].Value.ToString();
+                txtPrecioDeVentaProducto.Text = gridProductos.CurrentRow.Cells["precio_venta"].Value.ToString();
+
+                // Seleccionar los valores correctos en los ComboBox
+                cboMarca.Text = gridProductos.CurrentRow.Cells["marca"].Value.ToString();
+                cboRubro.Text = gridProductos.CurrentRow.Cells["rubro"].Value.ToString();
+                cboProveedor.Text = gridProductos.CurrentRow.Cells["proveedor"].Value.ToString();
+            }
+        }
+
+        // Evento para el botón Cargar Producto
+        private void btnCargarProducto_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(conexionBD))
+            {
+                conexion.Open();
+
+                string insertProducto = @"INSERT INTO productos 
+                                          (nombre, id_marca, id_rubro, id_proveedor, stock_actual, stock_minimo, precio_costo, precio_ganancia, precio_venta)
+                                          VALUES (@nombre, @id_marca, @id_rubro, @id_proveedor, @stock_actual, @stock_minimo, @precio_costo, @precio_ganancia, @precio_venta)";
+
+                MySqlCommand cmd = new MySqlCommand(insertProducto, conexion);
+                cmd.Parameters.AddWithValue("@nombre", txtNombreProducto.Text);
+                cmd.Parameters.AddWithValue("@id_marca", cboMarca.SelectedValue);
+                cmd.Parameters.AddWithValue("@id_rubro", cboRubro.SelectedValue);
+                cmd.Parameters.AddWithValue("@id_proveedor", cboProveedor.SelectedValue);
+                cmd.Parameters.AddWithValue("@stock_actual", txtStockActualProducto.Text);
+                cmd.Parameters.AddWithValue("@stock_minimo", txtStockMinimoProducto.Text);
+                cmd.Parameters.AddWithValue("@precio_costo", txtPrecioDeCostoProducto.Text);
+                cmd.Parameters.AddWithValue("@precio_ganancia", txtPorcentajeGananciaProducto.Text);
+                cmd.Parameters.AddWithValue("@precio_venta", txtPrecioDeVentaProducto.Text);
+
+                int filasAfectadas = cmd.ExecuteNonQuery();
+
+                if (filasAfectadas > 0)
+                {
+                    MessageBox.Show("Producto cargado exitosamente.");
+                    CargarProductos();  // Recargar los datos de productos
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo cargar el producto.");
+                }
+            }
+        }
+
+        // Evento para el botón Modificar Producto
+        private void btnModificarProducto_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtIdProducto.Text))
+            {
+                MessageBox.Show("Seleccione un producto para modificar.");
+                return;
+            }
+
+            using (MySqlConnection conexion = new MySqlConnection(conexionBD))
+            {
+                conexion.Open();
+
+                string updateProducto = @"UPDATE productos 
+                                          SET nombre = @nombre, id_marca = @id_marca, id_rubro = @id_rubro, 
+                                              id_proveedor = @id_proveedor, stock_actual = @stock_actual, 
+                                              stock_minimo = @stock_minimo, precio_costo = @precio_costo, 
+                                              precio_ganancia = @precio_ganancia, precio_venta = @precio_venta
+                                          WHERE id_producto = @id_producto";
+
+                MySqlCommand cmd = new MySqlCommand(updateProducto, conexion);
+                cmd.Parameters.AddWithValue("@nombre", txtNombreProducto.Text);
+                cmd.Parameters.AddWithValue("@id_marca", cboMarca.SelectedValue);
+                cmd.Parameters.AddWithValue("@id_rubro", cboRubro.SelectedValue);
+                cmd.Parameters.AddWithValue("@id_proveedor", cboProveedor.SelectedValue);
+                cmd.Parameters.AddWithValue("@stock_actual", txtStockActualProducto.Text);
+                cmd.Parameters.AddWithValue("@stock_minimo", txtStockMinimoProducto.Text);
+                cmd.Parameters.AddWithValue("@precio_costo", txtPrecioDeCostoProducto.Text);
+                cmd.Parameters.AddWithValue("@precio_ganancia", txtPorcentajeGananciaProducto.Text);
+                cmd.Parameters.AddWithValue("@precio_venta", txtPrecioDeVentaProducto.Text);
+                cmd.Parameters.AddWithValue("@id_producto", txtIdProducto.Text);
+
+                int filasAfectadas = cmd.ExecuteNonQuery();
+
+                if (filasAfectadas > 0)
+                {
+                    MessageBox.Show("Producto modificado exitosamente.");
+                    CargarProductos();  // Recargar los datos de productos
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo modificar el producto.");
+                }
+            }
+        }
+
+        // Evento para el botón Eliminar Producto
+        private void btnEliminarProducto_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtIdProducto.Text))
+            {
+                MessageBox.Show("Seleccione un producto para eliminar.");
+                return;
+            }
+
+            if (MessageBox.Show("¿Está seguro de que desea eliminar este producto?", "Confirmación", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (MySqlConnection conexion = new MySqlConnection(conexionBD))
+                {
+                    conexion.Open();
+
+                    string deleteProducto = "DELETE FROM productos WHERE id_producto = @id_producto";
+                    MySqlCommand cmd = new MySqlCommand(deleteProducto, conexion);
+                    cmd.Parameters.AddWithValue("@id_producto", txtIdProducto.Text);
+
+                    int filasAfectadas = cmd.ExecuteNonQuery();
+
+                    if (filasAfectadas > 0)
+                    {
+                        MessageBox.Show("Producto eliminado exitosamente.");
+                        CargarProductos();  // Recargar los datos de productos
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo eliminar el producto.");
+                    }
+                }
+            }
         }
     }
 }
